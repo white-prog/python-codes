4334b6c465ba4b0d8abad70bab7c1db45595eef9
white-prog <sabinhameed55@gmail.com>
2025-01-04 23:38:18 +0530

    small number pattern

---
+++ num_pattern.py
@@ -0,0 +1,18 @@
+def patternize(n):
+    for i in range(1,n+1):
+        for j in range(1,i):
+            print(j,end = " ")
+        print()
+
+def main():
+    print("NUMBER PATTERN HALF PYRAMID")
+    while True:
+        number = int(input("Enter number: "))
+        patternize(number)
+        choice = input("Type 'X' to exit: ")
+        if choice == 'X':
+            print("Good bye")
+            break
+
+if __name__ == "__main__":
+    main()
