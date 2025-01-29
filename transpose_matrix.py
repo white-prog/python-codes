43bf1074deaae4fc7560b14e6f86456ba3cadd7a
white-prog <sabinhameed55@gmail.com>
2025-01-29 23:33:42 +0530

    transpose of matrix

---
+++ transpose_matrix.py
@@ -0,0 +1,14 @@
+def matrix_change(matrix):
+    return matrix[::-1]
+
+def main():
+    mt = [[1,2,3],
+          [4,5,6],
+          [7,8,9]]
+    print(matrix_change[mt])
+
+if __name__ == "__main__":
+    main()
+
+
+            
