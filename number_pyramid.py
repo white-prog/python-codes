1c3219b492c99b3463cf5ffc12318e2734154f45
white-prog <sabinhameed55@gmail.com>
2025-01-31 00:12:05 +0530

    number pyramid

--- number_pyramid.py
+++ number_pyramid.py
@@ -1,1 +1,17 @@
 #number pyramid
+def num_pyr(num):
+    numbers = 1
+    for i in range(1,num+1):
+        spaces = " " * (num - i)
+        printer = f"{numbers}" * (2 * i- 1)
+        print(spaces + printer)
+        numbers += 2
+
+def main():
+    print("ODD NUMBER PYRAMID")
+    num_pyr(5)
+
+if __name__ == "__main__":
+    main()
+
+
